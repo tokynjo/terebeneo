7683eeb423224fe19cb5c20d2174d12c3f10cfe8
Nasolo RANDIANINA <nrandianina@bocasay.com>
2018-06-29 15:55:13 +0300

    validation log

---
+++ api/src/Entity/ValidationLog.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ValidationLogRepository")
+ */
+class ValidationLog
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $etape;
+
+    /**
+     * @var \Datetime
+     * @ORM\Column(name="created_at" ,type="datetime", nullable=true)
+     * @Gedmo\Timestampable(on="create")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Partner", inversedBy="validations", cascade={"persist"})
+     * @ORM\JoinColumn(name="partner_id", referencedColumnName="id")
+     */
+    private $partner;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getEtape(): ?int
+    {
+        return $this->etape;
+    }
+
+    public function setEtape(int $etape): self
+    {
+        $this->etape = $etape;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getPartner(): ?Partner
+    {
+        return $this->partner;
+    }
+
+    public function setPartner(?Partner $partner): self
+    {
+        $this->partner = $partner;
+
+        return $this;
+    }
+}

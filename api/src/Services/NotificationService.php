e4e1c222843db77a347079f7fa09d9844bdf4158
Nasolo RANDIANINA <nrandianina@bocasay.com>
2018-06-29 16:24:14 +0300

    entity

--- api/src/Services/NotificationService.php
+++ api/src/Services/NotificationService.php
@@ -7,11 +7,10 @@ use App\Entity\Partner;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Unirest\Request;
+
 /**
- * Created by PhpStorm.
- * User: rnasolo@gmail.com
- * Date: 24/10/2017
- * Time: 10:39
+ * Class NotificationService
+ * @package App\Services
  */
 class NotificationService
 {
@@ -21,12 +20,23 @@ class NotificationService
     protected $template;
     protected $mailer;
     protected $sms;
+    protected $frontUrl;
+    protected $apiUrl;
 
-    public function __construct(EntityManagerInterface $entityManager,$template, $mailer, $sms = null )
-    {
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        $template,
+        $mailer,
+        $sms = null,
+        $frontUrl = null,
+        $apiUrl = null
+    ){
         $this->em = $entityManager;
         $this->template = $template;
         $this->mailer = $mailer;
+        $this->sms = $sms;
+        $this->frontUrl = $frontUrl;
+        $this->apiUrl = $apiUrl;
     }
 
     public function sendNotification (Partner $partner, Notification $notification)
@@ -39,9 +49,9 @@ class NotificationService
                     $footer = '';
                     $mailContent = $content->getContent();
                     $parent = $partner->getParent();
-                    if ($parent->getHeadersFooters()){
-                        $header = $parent->getHeadersFooters()->getHeader() ? $parent->getHeadersFooters()->getHeader() : '';
-                        $footer = $parent->getHeadersFooters()->getFooter() ? $parent->getHeadersFooters()->getfooter() : '';
+                    if (!is_null($parent) && $parent->getActiveHeadersFooters()){
+                        $header = $parent->getActiveHeadersFooters()->getHeader() ? $parent->getActiveHeadersFooters()->getHeader() : '';
+                        $footer = $parent->getActiveHeadersFooters()->getFooter() ? $parent->getActiveHeadersFooters()->getfooter() : '';
                     }
                     $header = $this->replaceDataVars($partner, $header);
                     $footer = $this->replaceDataVars($partner, $footer);
@@ -56,7 +66,7 @@ class NotificationService
                         ]
                     );
                     $this->mailer->sendMailGrid(
-                        'Confirmation de création de compte Neobe',
+                        $content->getSubject() ? $content->getSubject() : 'Confirmation de création de compte Neobe',
                         [$partner->getMail()],
                         $template,
                         []
@@ -77,10 +87,46 @@ class NotificationService
      */
     protected function replaceDataVars(Partner $partner, $content)
     {
+        $pwdEncoder = new PasswordEncoder();
         foreach(Constant::$dataMailList as $key => $label) {
             switch ($key) {
+                //partner
                 case '__partenaire_nom_societe__' :
-                    $content = str_replace($key, $partner->getParent()->getName(), $content);
+                    $name = $partner->getName();
+                    if($partner->getParent()) {
+                        $name = $partner->getParent()->getName();
+                    }
+                    $content = str_replace($key, $name, $content);
+                    break;
+                case '__partenaire_nom__' :
+                    $lastName = $partner->getLastname();
+                    if($partner->getParent()) {
+                        $lastName = $partner->getParent()->getLastname();
+                    }
+                    $content = str_replace($key, $lastName, $content);
+                    break;
+                case '__partenaire_prenom__' :
+                    $firstName = $partner->getFirstname();
+                    if($partner->getParent()) {
+                        $firstName = $partner->getParent()->getFirstname();
+                    }
+                    $content = str_replace($key, $firstName, $content);
+
+                    break;
+
+                case '__partenaire_api_login__' :
+                    if($partner->getUser()) {
+                        $content = str_replace($key, $partner->getUser()->getEmail(), $content);
+                    }
+                    break;
+
+                case '__partenaire_api_mot_de_passe__' :
+                    if($partner->getUser()) {
+                        $content = str_replace($key, $partner->getUser()->getPlainPassword(), $content);
+                    }
+                    break;
+                case '__partenaire_api_url' :
+                    $content = str_replace($key, $this->apiUrl.'/create', $content);
                     break;
                 case '__client_nom__' :
                     $content = str_replace($key, $partner->getLastname(), $content);
@@ -98,14 +144,38 @@ class NotificationService
                     $content = str_replace($key, $partner->getVolumeSize(), $content);
                     break;
                 case '__url_create_account_validation__' :
-                    $frontUrl = $this->frontUrl.'/'.$partner->getHash();
+                    $frontUrl = "https://preprod-ter.dropcloud.fr/etape-1".'/'.$partner->getHash();
                     $content = str_replace($key, $frontUrl, $content);
+                    break;
+                case '__details_comptes_neobe__' :
+                    $details = '';
+                    if (sizeof($partner->getAccounts()) > 0) {
+                        foreach($partner->getAccounts() as $c) {
+                            $details .= '<ul>';
+                            $details .= '<li>Id : '.$c->getNeobeAccountId().'</li>';
+                            $details .= '<li>Login : '.$c->getLogin().'</li>';
+                            $details .= '<li>Mot de passe : '.$pwdEncoder->decode($c->getPassword()).'</li>';
+                            $details .= '<li>Espace total en Mo : '.$c->getTotalSize().'</li>';
+                            $details .= '<li>Espace utilisé en Mo : '.$c->getUsedSize().'</li>';
+                            $details .= '</ul>';
+                        }
+
+                    } else {
+                        $details .= '<p>Vous n\'avez pas encore activé de compte. Veuillez vous connecter sue votre éspace
+                        d\'administration Neobe pour le faire. </p>';
+                    }
+
+                    $content = str_replace($key, $details, $content);
+                    break;
+                case '__details_acces_neobe__' :
+                    $str = 'Id_client : '.$partner->getNeobeAccountId().'<br>';
+                    $str .= 'Email : '.$partner->getMail().'<br>';
+                    $str .= 'Mot de passe : '.$pwdEncoder->decode($partner->getPassword()).'<br>';
+                    $content = str_replace($key, $str, $content);
                     break;
             }
         }
 
         return $content;
     }
-
-
 }
---
+++ api/src/Services/PasswordEncoder.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Constants\Constant;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Class PasswordEncoder
+ * @package App\Services
+ */
+class PasswordEncoder
+{
+    const SERVICE_NAME = 'app.password';
+    const BEGIN_SALT = 5;
+    const SALT_LENGTH = 5;
+
+    private $seedings = [];
+
+
+    public function __construct()
+    {
+        $this->seedings = [
+            'alpha'     =>'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            'alphanum'  => '123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            'num'       => '0123456789',
+            'nozero'    => '123456789'
+            ];
+    }
+
+    /**
+     * @param string $type
+     * @param int $length
+     * @return string
+     */
+    public function random_str($type = 'alphanum', $length = 8)
+    {
+        switch($type)
+        {
+            case 'basic'    : return mt_rand();
+                break;
+            case 'alpha'    :
+            case 'alphanum' :
+            case 'num'      :
+            case 'nozero'   :
+                $str = '';
+                for ($i=0; $i < $length; $i++) {
+                    $str .= substr($this->seedings[$type], mt_rand(0, strlen($this->seedings[$type]) -1), 1);
+                }
+                return $str;
+                break;
+            case 'unique'   :
+            case 'md5'      :
+                return md5(uniqid(mt_rand()));
+                break;
+        }
+    }
+
+    /**
+     * @param string $password
+     * @param string $salt
+     * @return string
+     */
+    public function encode($password = '', $salt = '')
+    {
+        $salt = substr($salt, self::BEGIN_SALT, self::SALT_LENGTH).$salt;
+        $aSalt = str_split($salt, 1);
+        $aPwd = str_split($password, 1);
+        $pwdFinal = [];
+        foreach ($aSalt as $key => $value) {
+            array_push($pwdFinal,$value);
+            if (isset($aPwd[$key])) {
+                array_push($pwdFinal, $aPwd[$key]);
+            }
+        }
+        return base64_encode(implode('',$pwdFinal));
+    }
+
+    /**
+     * @param string $encoded
+     * @param string $salt
+     * @return string
+     */
+    public function decode($encoded = '')
+    {
+        $aEncoded = str_split(base64_decode($encoded), 1);
+        $pwd = [];
+        foreach($aEncoded as $key => $value ) {
+            if(($key % 2 == 1) && ($key < Constant::PASSWORD_LENGTH * 2)) {
+                $pwd[]= $value;
+            }
+        }
+        return implode($pwd);
+    }
+
+}

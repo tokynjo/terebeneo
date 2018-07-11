9a0029f4238c29bfc1c4507152fa5e98d06d4957
odilon <orafanambinantsoa@bocasay.com>
2018-07-11 11:02:03 +0200

    service SMS

--- client/src/Services/Sms.php
+++ client/src/Services/Sms.php
@@ -40,41 +40,41 @@ class Sms
      */
     public function send($numeros)
     {
-        $modelEMail = $this->entityManager->getRepository("App:EmailAutomatique")
-            ->findBy(['declenchement' => Constant::SEND_SMS], ['id' => 'DESC'], 1);
-        $data["receivers"] = [];
-        $data["failed"] = [];
-        if (isset($modelEMail[0])) {
-            $template = $modelEMail[0]->getTemplate();
-            $modele = ["__keyCrypt__"];
-            $real = [$folder->getCryptPassword()];
-            $template = str_replace($modele, $real, $template);
-            $smsApi = new SmsApi($this->ovhAppKey, $this->ovhAppSecret, "ovh-eu", $this->ovhConsumerKey);
-            $accounts = $smsApi->getAccounts();
-            $smsApi->setAccount($accounts[0]);
-            $senders = $smsApi->getSenders();
-            $message = $smsApi->createMessage();
-            $message->setSender($senders[0]);
-            $numTab = array_unique(preg_split("/(;|,)/", $numeros));
-            foreach ($numTab as $num) {
-                if (strpos($num, ' ') === 0) {
-                    $num = "+".trim($num);
-                }
-                if (preg_match("/^(\+|00)[1-9][0-9]{9,16}$/", $num) != 1) {
-                    $data["failed"] = trim($num);
-                } else {
-                    $message->addReceiver($num);
-                    $data["receivers"][] = $num;
-                }
-            }
-            $message->setIsMarketing(false);
-            $message->send(strip_tags($template));
-            $plannedMessages = $smsApi->getPlannedMessages();
-            foreach ($plannedMessages as $planned) {
-                $planned->delete();
-            }
-        }
-
-        return $data;
+//        $modelEMail = $this->entityManager->getRepository("App:EmailAutomatique")
+//            ->findBy(['declenchement' => Constant::SEND_SMS], ['id' => 'DESC'], 1);
+//        $data["receivers"] = [];
+//        $data["failed"] = [];
+//        if (isset($modelEMail[0])) {
+//           $template = $modelEMail[0]->getTemplate();
+//            $modele = ["__keyCrypt__"];
+//            $real = [$folder->getCryptPassword()];
+//            $template = str_replace($modele, $real, $template);
+//            $smsApi = new SmsApi($this->ovhAppKey, $this->ovhAppSecret, "ovh-eu", $this->ovhConsumerKey);
+//            $accounts = $smsApi->getAccounts();
+//            $smsApi->setAccount($accounts[0]);
+//            $senders = $smsApi->getSenders();
+//            $message = $smsApi->createMessage();
+//            $message->setSender($senders[0]);
+//            $numTab = array_unique(preg_split("/(;|,)/", $numeros));
+//            foreach ($numTab as $num) {
+//                if (strpos($num, ' ') === 0) {
+//                    $num = "+".trim($num);
+//                }
+//                if (preg_match("/^(\+|00)[1-9][0-9]{9,16}$/", $num) != 1) {
+//                    $data["failed"] = trim($num);
+//                } else {
+//                    $message->addReceiver($num);
+//                    $data["receivers"][] = $num;
+//                }
+//            }
+//            $message->setIsMarketing(false);
+//            $message->send(strip_tags($template));
+//            $plannedMessages = $smsApi->getPlannedMessages();
+//            foreach ($plannedMessages as $planned) {
+//                $planned->delete();
+//            }
+//        }
+//
+//        return $data;
     }
 }
